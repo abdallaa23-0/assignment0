03ea4f19d47a594774dbd5f6b542d8436a89ee4c
Your Name <abdalla.ali1136@gmail.com>
2026-09-13 20:05:28 -0600

    buttons

--- code/app/src/main/java/com/example/decisionmakingapp/MainActivity.kt
+++ code/app/src/main/java/com/example/decisionmakingapp/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.decisionmakingapp.ui.theme.DecisionMakingAppTheme
@@ -24,31 +28,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             DecisionMakingAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Greeting(name = "Abdalla")
+                        JapanTrip()
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     Text(
-        text = "Name: Abdalla Ali",
+        text = stringResource(R.string.nameLabel),
         modifier = modifier.padding(2.dp)
     )
     Text(
-        text = "CCID: abdalla5",
+        text = stringResource(R.string.ccidLabel),
         modifier = modifier.padding(33.dp)
     )
     Text(
-        text = "ID: 1848812",
+        text = stringResource(R.string.idLabel),
         modifier = modifier.padding(46.dp)
     )
 
 }
 
+@Composable
+fun JapanTrip(modifier: Modifier = Modifier){
+
+    Column(
+        modifier = modifier.padding(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Button(onClick = {/*TODO*/ }) {
+            Text(stringResource(R.string.yeah))
+        }
+    }
+}
